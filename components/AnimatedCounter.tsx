06a29c6c6395fd8dacc6dbd4cf4e008017f16790
user68354820 <kfablionel@gmail.com>
2024-08-16 17:55:51 +0200

    home layout

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,16 @@
+'use client';
+import CountUp from 'react-countup';
+
+const AnimatedCounter = ({ amount }:{ amount: number })  => {
+  return (
+    <div className="w-full">
+     <CountUp
+     duration={2}
+     decimal=","
+     prefix="Frw " 
+     end={amount}/> 
+    </div>
+  )
+}
+
+export default AnimatedCounter
