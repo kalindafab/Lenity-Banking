8ec272816f20d0e0ebd4a4db76f6681c4e421342
user68354820 <kfablionel@gmail.com>
2024-08-16 20:14:56 +0200

    implement MobileNAvigaton

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
     Sheet,
     SheetContent,
@@ -6,20 +7,73 @@ import {
     SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
-  
+import { Link } from "lucide-react"
+  import Image from 'next/image'
+import { sidebarLinks } from "../constants"
+import { cn } from "../lib/utils"
+import { usePathname } from 'next/navigation'
 const MobileNav = ({ user }: MobileNavProps) => {
+  const pathname=usePathname();
   return (
- <section>
+ <section className="w-fulll max-w-[264px]">
     <Sheet>
-  <SheetTrigger>Open</SheetTrigger>
-  <SheetContent>
-    <SheetHeader>
-      <SheetTitle>Are you absolutely sure?</SheetTitle>
-      <SheetDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </SheetDescription>
-    </SheetHeader>
+  <SheetTrigger>
+  <Image
+    src="/icons/hamburger.svg"
+    width={30}
+    height={30}
+    alt="menu"
+    className="cursor-pointer"
+    />
+  </SheetTrigger>
+ 
+  <SheetContent side="left" className="border-none bg-white">
+ 
+  <nav className="flex flex-col gap-4">
+            <Link href="/"
+            className="
+            cursor-pointer flex
+            items-center gap-2">
+                <Image 
+                src="/icons/logo.svg"
+                width={34}
+                height={34}
+                alt="Lenity logo"
+                
+                />
+                <h1 className="sidebar-logo">
+                    Lenity
+                </h1>
+            </Link>
+            {sidebarLinks.map((item) =>{
+                const isActive = pathname === item.route || pathname.startsWith(`{item.route}/`)
+                return (
+                    <Link href={item.route}key={item.label}
+                    className={cn
+                        ('sidebar-link', {
+                        'bg-bank-gradient':isActive
+                    })}
+                    >
+                        <div
+                        className='relative size-6'>
+                            <Image
+                            src={item.imgURL}
+                            alt={item.label}
+                            fill
+                            className={cn({
+                                'brightness-[3] invert-0':isActive
+                            })}
+                            />
+
+                        </div>
+                        <p className={cn('sidebar-label',{'!text-white':isActive})}>
+                        {item.label}
+                        </p>
+                        
+                    </Link>
+                )
+            })}
+            </nav>
   </SheetContent>
 </Sheet>
  </section>
