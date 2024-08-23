c0edd831e7d520218b1607837510d809c5c4d967
eazi201 <kfablionel@gmail.com>
2024-08-23 14:54:39 +0200

    modify next config

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -19,8 +19,10 @@ const CategoryBadge = ({ category}: CategoryBadgeProps)=>{
     } = transactionCategoryStyles[category as keyof typeof transactionCategoryStyles] || transactionCategoryStyles.default
     return(
         <div className={cn('category-badge',borderColor,chipBackgroundColor)}>
-            <div className={cn('size-2 rounded-full',backgroundColor)}/>
-                <p className={cn('text-[12px] font-medium',textColor)}>{category}</p>    
+            <div className={cn('size-2 rounded-full',backgroundColor)} >
+                <p className={cn('text-[12px] font-medium',textColor)}>{category}</p>
+            </div>
+
         </div>
     )
 }
